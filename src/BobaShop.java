2d3f7d86dbafa9c5e138ac285de6173ee7fc9b0d
BT_3W2_21 <sadiah16@nycstudents.net>
2023-11-22 12:34:57 -0500

    working

--- src/BobaShop.java
+++ src/BobaShop.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BobaShop {
+import java.util.Scanner;
+import
+public class BobaShop {
+    public void Boba() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Do you want milk tea or fruit tea? ");
+        String drinkChoice = scan.nextLine();
+        if (drinkChoice.equals("milk")) {
+            System.out.print("Do you want regular milk tea, taro milk tea, or brown sugar milk tea? ");
+            String milkChoice = scan.nextLine();
+        } else {
+            System.out.print("Do you want mango fruit tea, peach fruit tea, or strawberry fruit tea? ");
+            String fruitChoice = scan.nextLine();
+        }
+        System.out.print("What topping do you want? ");
+        String topping = scan.nextLine();
+    }
+
+    public String randomDrink() {
+        int random =
+    }
 }
